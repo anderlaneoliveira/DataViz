bea19c59452967cc6f32d15757bd5d20601b19ce
Anderlane Oliveira <73066557+anderlaneoliveira@users.noreply.github.com>
2021-06-15 10:17:17 -0300

    Add files via upload

---
+++ Dashboards_Utilizando_PowerBI.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# # Dashboards utilizando MS Power BI
+
+# ## 1. Dashboard
+
+# ![title](dados\dash01.jpg)
+
+# ## 2. Dashboard
+
+# ![title](dados\dash01.jpg)
+
+# ## 3. Dashboard
+
+# ![title](dados\dash01.jpg)
+
+# ## 4. Dashboard
+
+# ![title](dados\dash01.jpg)
+
+# In[ ]:
+
+
+
+
